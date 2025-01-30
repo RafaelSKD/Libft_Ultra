6d669976f0945163b1076e80a51633dfc2a596b9
RafaelSKD <rcosta.skd@gmail.com>
2025-01-30 15:40:56 +0000

    301

--- src/str/ft_strjoin.c
+++ src/str/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: rafaefer <rafaefer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:40:50 by rafaefer          #+#    #+#             */
-/*   Updated: 2024/04/22 16:28:14 by rafaefer         ###   ########.fr       */
+/*   Updated: 2025/01/30 15:39:43 by rafaefer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,16 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	j;
 
 	str = (char *)malloc(
-			sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+			sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
 		return (NULL);
 	i = 0;
 	j = 0;
 	while (s1[i])
-	{
-		str[j++] = s1[i];
-		i++;
-	}
+		str[j++] = s1[i++];
 	i = 0;
 	while (s2[i])
-	{
-		str[j++] = s2[i];
-		i++;
-	}
-	str[j] = 0;
+		str[j++] = s2[i++];
+	str[j] = '\0';
 	return (str);
 }
